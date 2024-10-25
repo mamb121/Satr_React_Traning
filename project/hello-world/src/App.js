e372a1efcb12f12356a21cb090c73ea2ccc4483c
Moahmmed Badahman <mamb121@hotmail.com>
2024-10-25 22:02:41 +0300

    using axios with class component

--- project/hello-world/src/App.js
+++ project/hello-world/src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      imageurl:''
+    }
+  }
+  componentDidMount(){
+    this.callApi()
+  }
     callApi = ()=>{
-        axios.get("")
+        axios.get("https://api.thecatapi.com/v1/images/search")
+        .then((response) =>{
+          console.log(response.data[0].url)
+          this.setState({imageurl: response.data[0].url})
+        }).catch((error)=>{
+          console.log(error)
+        })
     }
   render() {
     return (
       <div>
         Thid Party API
+        <div>
+          the image url is : {this.state.imageurl}
+          <br></br>
+          <img src={this.state.imageurl} alt='cat' width='100' height='100'/>
+        </div>
       </div>
     )
   }
